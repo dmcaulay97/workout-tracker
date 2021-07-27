3b39a486f567946c39df4dde553cdbb2aa6e3f82
Dylan McAulay <dmcaulay97@gmail.com>
2021-07-27 12:52:53 -0400

    adding first route/ no data being returned with find

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const { db } = require("./models/workoutSchema");
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,17 +18,16 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
 
 app.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).sort({ day: 1 }, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
+    db.Workout.find({})
+        .then(data => {
             console.log(data);
-            res.json(data);
-        }
-    })
-})
-
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        });
+});
 
 app.listen(PORT, () => {
-    console.log("App running on port 3000!");
+    console.log("App running on port 3001!");
 });
