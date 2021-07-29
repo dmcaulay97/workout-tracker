8aa1ad6877e05e6347b1dd260f1bb4dea24f89d0
Dylan McAulay <dmcaulay97@gmail.com>
2021-07-29 14:36:41 -0400

    added all routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mongodb = require("mongodb");
 
 const PORT = process.env.PORT || 3001;
 
 const db = require("./models");
+const { Router } = require("express");
 
 const app = express();
 
@@ -23,6 +25,9 @@ app.get("/api/workouts", (req, res) => {
             $addFields: {
                 totalDuration: { $sum: "$exercises.duration" }
             }
+        },
+        {
+            $sort: { "day": 1 }
         }
     ])
         .then((data) => {
@@ -34,6 +39,58 @@ app.get("/api/workouts", (req, res) => {
             res.json(err)
         });
 });
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        },
+        {
+            $sort: { "day": -1 }
+        },
+        {
+            $limit: 7
+        }
+    ])
+        .then((data) => {
+            console.log(data);
+            console.log(data[0].exercises)
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        });
+})
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.updateOne(
+        {
+            _id: mongodb.ObjectId(req.params.id)
+        },
+        {
+            $push: { exercises: req.body }
+        }
+    )
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
 
 app.listen(PORT, () => {
     console.log("App running on port 3001!");
