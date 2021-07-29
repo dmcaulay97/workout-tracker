91f1b4af42de30cacd98eea8b793fa9d48fb16a4
Dylan McAulay <dmcaulay97@gmail.com>
2021-07-29 16:23:59 -0400

    adding mongdb connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const mongodb = require("mongodb");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,7 +18,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
 
 app.get("/api/workouts", (req, res) => {
     db.Workout.aggregate([
